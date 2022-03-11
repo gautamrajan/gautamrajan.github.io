6c6d4418b77a45b2efe570a23ee6f2da7296143e
gautamrajan <grajan@usc.edu>
2022-03-10 18:34:51 -0800

    initial commit of website files

---
+++ grajanwebsite/src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import './app.css'
+  import {onMount} from 'svelte';
+  import MainText from './lib/MainText.svelte'
+  
+</script>
+<main>
+  <MainText/> 
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+
+</style>
---
+++ grajanwebsite/src/routes/blackjack.svelte
@@ -0,0 +1,7 @@
+src/routes/blackjack.svelte
+<svelte:head>
+  <title>Blackjack</title>
+</svelte:head>
+
+<h1>Welcome to the casino friendo</h1>
+<p>TODO...</p>
